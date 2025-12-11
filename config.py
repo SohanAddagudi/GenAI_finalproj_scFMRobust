8a1151621314344f1beaf5d7153ac2c264f3b61d
Sohan Addagudi <115036581+SohanAddagudi@users.noreply.github.com>
2025-12-10 20:02:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ BATCH_COL = "batch"
 SUBSAMPLE_RATIOS = [1.0, 0.5, 0.1]
 DROPOUT_RATES = [0.0, 0.2, 0.4, 0.6]
 
-BATCH_GENES = 500
-BATCH_SHIFT = 2.0
+BATCH_GENE_SIGMA = 0.4
+BATCH_LIBRARY_FACTOR = 0.7
 
 MODELS = ["PCA", "scGPT", "Geneformer", "scFoundation"]
