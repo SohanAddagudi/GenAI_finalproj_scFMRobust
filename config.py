e9a11d3111b59d558cbb0750f4a015dbbbd34a6d
Sohan Addagudi <115036581+SohanAddagudi@users.noreply.github.com>
2025-12-10 19:57:36 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,12 @@ RESULTS_DIR = "./results"
 
 RANDOM_SEED = 42
 CELL_TYPE_COL = "cell_type"
+BATCH_COL = "batch"
 
 SUBSAMPLE_RATIOS = [1.0, 0.5, 0.1]
 DROPOUT_RATES = [0.0, 0.2, 0.4, 0.6]
+
+BATCH_GENES = 500
+BATCH_SHIFT = 2.0
 
 MODELS = ["PCA", "scGPT", "Geneformer", "scFoundation"]
