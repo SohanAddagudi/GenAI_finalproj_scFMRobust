0246c6c0997d579cae15bf1f096e04b208324364
Sohan Addagudi <115036581+SohanAddagudi@users.noreply.github.com>
2025-12-10 19:29:51 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+RAW_DATA_PATH = "./data/pbmc3k_processed.h5ad"
+EMBEDDING_DIR = "./embeddings"
+RESULTS_DIR = "./results"
+
+RANDOM_SEED = 42
+CELL_TYPE_COL = "cell_type"
+
+SUBSAMPLE_RATIOS = [1.0, 0.5, 0.1]
+DROPOUT_RATES = [0.0, 0.2, 0.4, 0.6]
+
+MODELS = ["PCA", "scGPT", "Geneformer", "scFoundation"]
